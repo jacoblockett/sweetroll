a19b5d58a5d390418452761753b047f28493e20b
jacoblockett <jacobwyattlockett@gmail.com>
2023-12-04 20:11:56 -0500

    added some type checks

--- utils/getType.js
+++ utils/getType.js
@@ -1,10 +1,14 @@
 export default value => {
-	if (typeof value === "boolean") return "boolean"
-	if (value instanceof Map) return "map"
-	if (typeof value === "function") return "function"
-	if (typeof value === "number") return "number"
-	if (value instanceof Set) return "set"
 	if (typeof value === "string") return "string"
+	if (typeof value === "number" && !isNaN(value)) return "number"
+	if (typeof value === "boolean") return "boolean"
+	if (typeof value === "function") return "function"
+	if (typeof value === "symbol") return "symbol"
+	if (typeof value === "bigint") return "bigint"
+	if (value instanceof Map) return "map"
+	if (value instanceof Set) return "set"
+	if (value instanceof WeakMap) return "weakmap"
+	if (value instanceof WeakSet) return "weakset"
 
 	return Object.prototype.toString.call(value).toLowerCase().slice(8, -1)
 }
