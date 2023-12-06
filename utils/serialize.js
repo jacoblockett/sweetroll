432ccbde2d3aec93a3447cae2dce475b16e107e3
jacoblockett <jacobwyattlockett@gmail.com>
2023-12-06 00:56:06 -0500

    found a stick bug. snapped it.

--- utils/serialize.js
+++ utils/serialize.js
@@ -1,10 +1,10 @@
-import serializeFunctionsInObject from "./serializeFunctionsInObject.js"
+import serializeObject from "./serializeObject.js"
 import packer from "./packer.js"
 
 export default data => {
 	if (typeof data === "function") return data.toString()
 	if (Object.prototype.toString.call(data) === "[object Object]")
-		return packer.pack(serializeFunctionsInObject(data)).toString()
+		return packer.pack(serializeObject(data)).toString()
 
 	return data
 }
